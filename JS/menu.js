45fde8686fa760d2f6312f4d12bb4f52852e1ce4
MateuszMi <mati_michna@vp.pl>
2021-02-14 12:42:12 +0100

    hideMenu - click

--- JS/menu.js
+++ JS/menu.js
@@ -23,103 +23,25 @@ window.onscroll = function() {
     let currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
         document.getElementById("navbar").style.top = "0";
+        menuLinks.classList.remove('active');
+        menu.classList.remove("is-active");
     } else {
         document.getElementById("navbar").style.top = "-80px";
+        menuLinks.classList.remove('active');
+        menu.classList.remove("is-active");
     }
     prevScrollpos = currentScrollPos;
 
 };
 
+const closeMenu = document.getElementById("hideMenu")
+closeMenu.onclick = function() {
+    menuLinks.classList.remove('active');
+    menu.classList.remove("is-active");
+}
 
-const blog = document.querySelector('.blog');
-const cards = document.querySelector('.cards');
-const client = document.querySelector('.client');
-const contact = document.querySelector('.contact');
-const conteiner = document.querySelector('.conteiner');
-const experience = document.querySelector('.experience');
-const fo = document.querySelector('.focus');
-const slider = document.querySelector('.slider')
-const guide = document.querySelector('.guide');
-const industries = document.querySelector('.industries');
-const news = document.querySelector('.news')
-
-
-blog.addEventListener('click', function(e) {
-    if (e.target.classList.contains('blog')) {
-        menuLinks.classList.remove('active');
-        menu.classList.remove("is-active");
-
-    }
-});
-cards.addEventListener('click', function(e) {
-    if (e.target.classList.contains('cards')) {
-        menuLinks.classList.remove('active');
-        menu.classList.remove("is-active");
-
-    }
-});
-client.addEventListener('click', function(e) {
-    if (e.target.classList.contains('client')) {
-        menuLinks.classList.remove('active');
-        menu.classList.remove("is-active");
-
-    }
-});
-contact.addEventListener('click', function(e) {
-    if (e.target.classList.contains('contact')) {
-        menuLinks.classList.remove('active');
-        menu.classList.remove("is-active");
-
-    }
-});
-conteiner.addEventListener('click', function(e) {
-    if (e.target.classList.contains('conteiner')) {
-        menuLinks.classList.remove('active');
-        menu.classList.remove("is-active");
-
-    }
-});
-experience.addEventListener('click', function(e) {
-    if (e.target.classList.contains('experience')) {
-        menuLinks.classList.remove('active');
-        menu.classList.remove("is-active");
-
-    }
-});
-
-fo.addEventListener('click', function(e) {
-    if (e.target.classList.contains('focus')) {
-        menuLinks.classList.remove('active');
-        menu.classList.remove("is-active");
-
-    }
-});
-slider.addEventListener('click', function(e) {
-    if (e.target.classList.contains('slider')) {
-        menuLinks.classList.remove('active');
-        menu.classList.remove("is-active");
-
-    }
-});
-guide.addEventListener('click', function(e) {
-    if (e.target.classList.contains('guide')) {
-        menuLinks.classList.remove('active');
-        menu.classList.remove("is-active");
-
-    }
-});
-
-industries.addEventListener('click', function(e) {
-    if (e.target.classList.contains('industries')) {
-        menuLinks.classList.remove('active');
-        menu.classList.remove("is-active");
-
-    }
-});
-news.addEventListener('click', function(e) {
-    if (e.target.classList.contains('news')) {
-        menuLinks.classList.remove('active');
-        menu.classList.remove("is-active");
-
-    }
-});
+const closeMenuSlider = document.getElementById("slider")
+closeMenuSlider.onclick = function() {
+    menuLinks.classList.remove('active');
+    menu.classList.remove("is-active");
+}
